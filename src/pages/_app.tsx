01a4e68900f82a5a6326da594cbe11509430fc8b
eunji <eunjijjang11@naver.com>
2023-01-05 11:13:46 +0900

    style: global style 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from 'next/app'
 import Link from 'next/link'
+import GlobalStyle from '../../styles/GlobalStyle'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <GlobalStyle />
       <nav>
-        <Link href={'/'}>main</Link>
-        <Link href={'/login'}>login</Link>
+        <Link href={'/'}>main </Link>
+        <Link href={'/login'}>login </Link>
         <Link href={'/signup'}>signup</Link>
       </nav>
       <Component {...pageProps} />
